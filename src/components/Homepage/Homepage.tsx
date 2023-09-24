0492391ec03cf64ee92bdffc8cd7c3181c1b3f27
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-24 01:31:40 -0500

    finished layout for homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,22 +1,63 @@
 import React from "react";
 import "./Homepage.css";
+import Typography from "@mui/material/Typography";
+import NavBar from "./Appbar/NavBar";
+import Button from "@mui/material/Button";
 
 export default function Homepage() {
   return (
-    <div id="homepage">
-      <div>
-        <h1>Hello! My name is Jiaxi!</h1>
-        <h2>
-          I am a Full Stack Developer with over 4 years of professional
-          experience.
-        </h2>
-        <h2>
-          I have a passion for building elegant applications and digital
-          experiences.
-        </h2>
-      </div>
-      <div>
-        <h1> PICTURES</h1>
+    <div>
+      <NavBar />
+      <div id="homepage">
+        <div id="text">
+          <Typography
+            variant="h1"
+            sx={{
+              mr: 0,
+              display: { xs: "flex", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            <h1 id="intro">Hello! My name is Jiaxi!</h1>
+          </Typography>
+          <Typography
+            sx={{
+              mr: 0,
+              display: { xs: "flex", md: "flex" },
+              fontFamily: "monospace",
+
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            <h2 className="info">
+              I am a Full Stack Developer with over 4 years of professional
+              experience.
+            </h2>
+          </Typography>
+          <Typography
+            sx={{
+              mr: 0,
+              display: { xs: "flex", md: "flex" },
+              fontFamily: "monospace",
+
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            <h2 className="info">
+              Passionate about building elegant applications and digital
+              experiences.
+            </h2>
+          </Typography>
+          <Button variant="contained" color="error">
+            Projects
+          </Button>
+        </div>
       </div>
     </div>
   );
