6fab41617a02f2f2e4fa6abc9a65d238ad7ec080
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-09-25 23:42:34 -0500

    meditations app linked

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,8 +10,16 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import "./projects.css";
 import { data } from "./projectsData";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
+  const navigate = useNavigate();
+
+  const goToWebpage = (data) => {
+    window.location.replace(data.url);
+  };
+
   return (
     <div>
       <h1 id="projects"> Projects</h1>
@@ -24,26 +32,28 @@ export default function Projects() {
       >
         {data.map((product, key) => (
           <Grid xs={2} sm={4} md={4} key={key}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={product.picture}
-                  alt={product.alt}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {product.title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <Link to={product.url}>
+              <Card sx={{ maxWidth: 345 }} onClick={goToWebpage}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={product.picture}
+                    alt={product.alt}
+                    url={product.url}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {product.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      <div>{product.details}</div>
+                      <div> {product.tools}</div>
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
           </Grid>
         ))}
       </Grid>
